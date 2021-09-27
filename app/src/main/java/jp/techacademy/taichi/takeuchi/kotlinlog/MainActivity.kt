adeeab0aea7c710012454b5eba90b2876508be0e
takeuchi-hn <sinnichi406t.t@gmail.com>
2021-09-27 23:36:46 +0900

    9/27

--- app/src/main/java/jp/techacademy/taichi/takeuchi/kotlinlog/MainActivity.kt
+++ app/src/main/java/jp/techacademy/taichi/takeuchi/kotlinlog/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("kotlintest", "ログへの出力テスト") ; Log.d("kotlintest", "ログ2")   // ここを追加
+        Log.d("kotlintest", "ログへの出力テスト") ; Log.d("kotlintest", "ログ2")
 
         //
         var num = 10
@@ -17,6 +17,32 @@ class MainActivity : AppCompatActivity() {
 
         num =50
         Log.d("kotlintest",num.toString())
+
+        val num1 = 10 + 5 - 2 * 4 / 2
+        Log.d("kotlintest", "10 + 5 - 2 * 4 / 2 = " + num1)
+
+        val flag1 = true
+        val flag2 = false
+        Log.d("kotlintest", "flag1 && flag2 = " + (flag1 && flag2))
+        Log.d("kotlintest", "flag1 || flag2 = " + (flag1 || flag2))
+
+        val num2 = 10
+        val num3 = 20
+        Log.d("kotlintest", "num2 < num3 = " + (num2 < num3))
+
+
+        //  条件分岐
+        num = 60
+
+        if (num >= 90) {
+            Log.d("kotlintest", "優")
+        } else if (num >= 75) {
+            Log.d("kotlintest", "良")
+        } else if (num >= 60) {
+            Log.d("kotlintest", "可")
+        } else {
+            Log.d("kotlintest", "不可")
+        }
 
     }
 }
